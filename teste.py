e65c8467475895bd38cad4e2c1c0a9f4d2258522
marianadesafigueiredo-collab <marianadesafigueiredo@gmail.com>
2025-10-06 15:18:55 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,9 +1,6 @@
 import streamlit as st 
 
+st.title("Meu programa")
 st.write("Alô mundo")
 
-import streamlit as st
 
-st.title("Exibindo uma imagem no Streamlit")
-
-st.image("caminho/para/sua/imagem.jpg", caption="Uma imagem de exemplo", width=300)
