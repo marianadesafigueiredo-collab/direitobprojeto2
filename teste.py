2f7bca1f9b8e8634cb5197c8338f9782bc4fe5ea
marianadesafigueiredo-collab <marianadesafigueiredo@gmail.com>
2025-10-06 15:17:43 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,9 +4,6 @@ st.write("Alô mundo")
 
 import streamlit as st
 
-# Define o título da aplicação
 st.title("Exibindo uma imagem no Streamlit")
 
-# Adiciona uma imagem ao aplicativo
-# Certifique-se de que o caminho 'caminho/para/sua/imagem.jpg' está correto
 st.image("caminho/para/sua/imagem.jpg", caption="Uma imagem de exemplo", width=300)
