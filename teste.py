71ac5913a037ef6c5614ca9da5deaa834c21a179
marianadesafigueiredo-collab <marianadesafigueiredo@gmail.com>
2025-10-12 21:21:31 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -10,3 +10,18 @@ if nome:
 idade= st.text_input("Digite sua idade:")
 if idade:
   st.write (idade)
+
+cargo= st.text_input("Digite o seu cargo:")
+try:
+    salario= float(st.text_input("Digite o seu salário:"))
+    erro = False
+except:
+    st.write("O salário deve ser um número")
+    erro = True
+
+if not erro:
+  if cargo.lower().startswith == 'gerente':
+   novo_salario= salario * 1.05
+  else:
+   novo_salario= salario * 1.07
+st.write("O novo salário é ", novo_salario)
