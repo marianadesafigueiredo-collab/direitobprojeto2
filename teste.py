50fb4094415fd74224883992f08c6954e2f14d04
marianadesafigueiredo-collab <marianadesafigueiredo@gmail.com>
2025-10-12 21:42:07 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -11,17 +11,5 @@ idade= st.text_input("Digite sua idade:")
 if idade:
   st.write (idade)
 
-cargo= st.text_input("Digite o seu cargo:")
-try:
-    salario= float(st.text_input("Digite o seu salário:"))
-    erro = False
-except:
-    st.write("O salário deve ser um número")
-    erro = True
-
-if not erro:
-  if cargo.lower().startswith == 'gerente':
-   novo_salario= salario * 1.05
-  else:
-   novo_salario= salario * 1.07
-st.write("O novo salário é ", novo_salario)
+frase= nome +" é uma pessoa legal"
+st.write(frase)
