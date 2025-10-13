db0fa071c5c13d115cf7e213d2eb403178ae2c63
marianadesafigueiredo-collab <marianadesafigueiredo@gmail.com>
2025-10-13 15:00:58 -0300

    Update paises.py

--- paises.py
+++ paises.py
@@ -5,3 +5,12 @@ dataset = pd.read_csv("https://www.irdx.com.br/media/uploads/paises.csv")
 
 st.dataframe(dataset)
 
+
+import plotly.express as px
+fig= px.scatter_geo(dataset,
+                    lat=dataset['latitude'],
+                    lon=dataset['longitude'],
+                    hover_name=dataset['nome'])
+fig.update_layout(title='Coordenadas dos países no mapa',
+                  geo_scope= 'world')
+fig.show()
